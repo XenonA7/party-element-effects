0bd94a80bcbbf32eb61cabd0ea5291232ff52833
XenonA7 <105614278+XenonA7@users.noreply.github.com>
2024-11-08 11:17:26 -0700

    1.1.0
    New features by Azure Lazuline

---
+++ changelog.md
@@ -0,0 +1,12 @@
+## 1.1.0 (11/08/2024)
+- Additional features by Azure Lazuline:
+ * Party members switch back to neutral after combat ends
+ * Element aura particle effect now works like Lea's (only shows while character is moving)
+- Added changelog.md
+
+## 1.0.1 (02/19/2024)
+- Updated ccmod.json to new standard
+
+
+## 1.0.0 (06/07/2022)
+- Initial Release
--- prestart.js
+++ prestart.js
@@ -1,12 +1,56 @@
 sc.PartyMemberEntity.inject({
-	updateElement: function () {
+  updateElement: function () {
       if (this.model) {
         var a = this.getBestElement();
         if (this.model.currentElementMode != a) {
-	        sc.combat.showModeChange(this, a);
-	        sc.combat.showModeAura(this, a)
-	      }
+          sc.combat.showModeChange(this, a);
+          //sc.combat.showModeAura(this, a);
+        }
         a != this.model.currentElementMode && this.model.setElementMode(a);
       }
     }
+});
+
+sc.PartyMemberEntity.inject({
+  update(...args) {
+  if(!sc.model.isCombatMode() && !sc.model.isCombatCooldown())
+  { //if outside combat, and not in the cooldown before combat *actually* cancels
+    if(this.model.currentElementMode != 0 && this.timeUntilNeutral == null)
+    {
+      this.timeUntilNeutral = 0.5 + Math.random() * 2; //random between 0.5 and 2.5 seconds before reverting
+    }
+    if(this.timeUntilNeutral != null)
+    { //decrement the counter
+      this.timeUntilNeutral -= 1 * ig.system.tick;
+      if(this.timeUntilNeutral <= 0) //if outside combat, and the timer has hit zero, swap back to neutral
+      {
+        sc.combat.showModeChange(this, 0); //hook this one line up to a toggle option to disable the neutral element animation maybe?
+        this.model.currentElementMode = 0;
+        this.timeUntilNeutral = null;
+      }
+    }
+  }
+  else
+    this.timeUntilNeutral = null;
+  
+  
+  var speedmagnitude = Math.sqrt(this.coll.vel.x*this.coll.vel.x + this.coll.vel.y*this.coll.vel.y);
+  if (speedmagnitude > 50)
+    this.movingtime=0.5; //if they're moving, set a timer for 0.5 seconds
+  if (this.movingtime == null)
+    this.movingtime=0;
+  this.movingtime -= 1 * ig.system.tick;
+
+  var auratype = this.model.currentElementMode;
+  if (this.movingtime < 0) //if it's been over 0.5 seconds since they moved, override the aura type to be nothing
+    auratype = 0;
+  
+  if(this.lastauratype == null || this.lastauratype != auratype) //if the new aura type is different from what's active, swap it
+  {
+    this.lastauratype = auratype;
+    sc.combat.showModeAura(this, auratype);
+  }
+    
+  this.parent(...args);
+  },
 });
